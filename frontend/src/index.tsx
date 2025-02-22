4cd5cfac61e883d56582105e0a39a72fe7381d33
William Nolin <william.nolin.1@ens.etsmtl.ca>
2025-02-21 20:13:58 -0500

    Partially working injection into GitHub

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,14 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppWrapper from "./App";
 
-// Only used in the extension
-const root = document.createElement("div");
-document.body.appendChild(root);
+const rootContainerId = "cscope-root";
 
-const rootDiv = ReactDOM.createRoot(root);
+const createRoot = () => {
+  let root = document.getElementById(rootContainerId);
+  if (!root) {
+    // The extension will create a root container for the app.
+    // If it was not found, we are probably running the app standalone and we need to create the container.
+    root = document.createElement("div");
+    root.id = rootContainerId;
+    document.body.appendChild(root);
+  }
 
-rootDiv.render(
-  <React.StrictMode>
-    <AppWrapper />
-  </React.StrictMode>
-);
+  const rootDiv = ReactDOM.createRoot(root);
+  rootDiv.render(
+    <React.StrictMode>
+      <AppWrapper />
+    </React.StrictMode>
+  );
+};
+
+if (!!chrome && !!chrome.runtime) {
+  chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+      console.log("Request received");
+    }
+  );
+}
+
+createRoot();
